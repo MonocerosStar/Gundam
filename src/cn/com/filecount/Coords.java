845726ff5df021364e7da62ef443e58e466f8f4e
MonocerosStar <76774278+MonocerosStar@users.noreply.github.com>
2021-07-23 17:50:19 +0800

    [20210719]fix@wangliuyang:完善统计字符出现次数，位置接口

---
+++ src/cn/com/filecount/Coords.java
@@ -0,0 +1,24 @@
+package cn.com.filecount;
+
+public class Coords {
+	int x;
+	int y;
+	public Coords(int x, int y) {
+		// TODO Auto-generated constructor stub
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+}
--- src/cn/com/filecount/CountUtils.java
+++ src/cn/com/filecount/CountUtils.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
@@ -12,23 +14,35 @@ public class CountUtils {
 
 	private Map getCharNums(String fileName,String letter) throws IOException{
 	int count = 0 ;
+	//记录行数行数、列数和次数-用来对应坐标
 	int rowLocation = 0; 
 	int colLocation = 0;
+	int counts = 0;
+	//用来存放字符出现的总次数
 	Map<String,Integer> map = new HashMap<String,Integer>();
+	Map<String,Object> ret = new HashMap<String,Object>();
 	File file = new File(fileName);
-	//记录行数line
 	String line = null;
+	List<Coords> list =new ArrayList<Coords>();
 	if (file.exists() && letter != null) {
 		//读文件 file
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		while ((line = br.readLine()) != null) {
-			Pattern p = Pattern.compile("\\s*|\\t|\\r|\\n");
-			
+			rowLocation++ ;
+			while (line.contains(letter)) {
+				colLocation = line.indexOf(letter);
+				list.add(new Coords(rowLocation,colLocation));
+				line = line.substring(line.indexOf(letter));
+				counts++;	
+			}		
 		}
+		map.put(letter, counts);
+		ret.put(letter, map);
+		ret.put("location", list);
+		br.close();
 	}else {
 		System.out.println("文件不存在，或统计的字母不存在！");
 	}
-	return map;
-	
+	return ret;
 }
 }
