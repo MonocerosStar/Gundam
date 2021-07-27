f5b7f1e3ebdc71b519307c5fa284df70091d229a
MonocerosStar <76774278+MonocerosStar@users.noreply.github.com>
2021-07-27 16:25:07 +0800

    [20210721]feat@wangliuyang:编写测试案例，修复Bug

---
+++ src/cn/com/filecount/Test.java
@@ -0,0 +1,43 @@
+package cn.com.filecount;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+
+public class Test {
+	public static void main(String[] args) throws IOException{
+		CountUtils util = new CountUtils();
+		System.out.println("-w 返回文件 file.c 中指定的字符的数目和位置  \n" +"-l 返回文件 file.c 的总行数 \n"+"-x 返回文件 file.c 的行数/注释行数/空行数");
+		System.out.println("-o 文件路径 (生成result文件)\n");
+		System.out.println("----------------------------------------------------------------------------------------------------------");
+		System.out.println("请输入命令：（格式为：命令 文件路径 需要查询的字符）");
+		while(true){
+			//流处理处理输入的文件数据
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String input=br.readLine();
+			//将输入以空格进行分割,command分割成数组进行存储工作
+			String[] commond = input.split(" ");
+			//使用switch输入的命令进行分割
+			switch(commond[0]) {
+			case "-w": 
+				Map resMap = (Map)util.getCharNums(commond[1],commond[2]);
+				System.out.println("字符数：");
+				break;
+			case "-l":
+				int num = util.getLineNumber(commond[1]);
+				System.out.println("行数："+num);
+				break;
+			case "-x":
+				util.countDiffLine(commond[1]);
+				break;
+			case"-o":
+				util.outFile(commond[1]);
+				break;
+			default:
+				System.out.println("命令不正确,请重新输入!");
+				break;
+			}
+		}
+	}
+}
