89e2f9dec5f65852d6353d32654c59129446e164
Monoceros <76774278+MonocerosStar@users.noreply.github.com>
2021-08-06 19:31:35 +0800

    [20210806]feat@wly:新增分数类的构造方法、求最大公约数和简化方法

--- src/cn/com/java/fraction/Fraction.java
+++ src/cn/com/java/fraction/Fraction.java
@@ -1,5 +1,39 @@
 package cn.com.java.fraction;
-
+/**
+ * 分数计算类
+ * @author Monoceros
+ *
+ */
 public class Fraction {
-
+	public int molecule;//分子
+	public int denominator;//分母
+	public Fraction(int molecule, int denominator) {
+		if (denominator == 0) {
+			try {
+				throw new Exception("denominator cannot be zero!");
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		this.molecule = molecule;
+		this.denominator = denominator;
+	}
+	//欧几里得法求最大公约数
+	public int findMaxDivisior(int num1, int num2) {
+		int comDivisor1;
+		int comDivisor2;
+		if (num1 > num2) {
+			comDivisor1 = Math.abs(num1);
+			comDivisor2 = Math.abs(num2);
+		}else {
+			comDivisor2 = Math.abs(num1);
+			comDivisor1 = Math.abs(num2);
+		}
+		int remainder = comDivisor1 % comDivisor2;
+		if (remainder == 0) 
+			return comDivisor2;
+		else 
+			return findMaxDivisior(comDivisor2,remainder);
+	}
 }
